08fd1b9612e1790f9d8c8f36af6cab7bd47d7f8e
pdoazan <paul.doazan@gmail.com>
2023-02-28 13:46:43 +0100

    WIP

--- easy_exercices/01/src/main.js
+++ easy_exercices/01/src/main.js
@@ -1,15 +1,66 @@
 const colors = ['#063c77', '#f19648', '#f5d259', '#d84f35']
 
+let cities = [
+    {
+        name: 'Paris',
+        superficy: 104.5,
+        population: 10858874
+    },
+    {
+        name: 'Londres',
+        superficy: 1572,
+        population: 8908081
+    },
+    {
+        name: 'Madrid',
+        superficy: 608,
+        population: 3280782
+    },
+    {
+        name: 'Lisbonne',
+        superficy: 100.05,
+        population: 509515
+    },
+    {
+        name: 'Rome',
+        superficy: 2213,
+        population: 2844395
+    },
+    {
+        name: 'Berlin',
+        superficy: 891.7,
+        population: 3748148
+    },
+    {
+        name: 'Moscou',
+        superficy: 2542,
+        population: 12655050
+    },
+    {
+        name: 'Varsovie',
+        superficy: 546,
+        population: 1793579
+    },
+    {
+        name: 'Copenhague',
+        superficy: 292,
+        population: 1345562
+    },
+    {
+        name: 'Dublin',
+        superficy: 318,
+        population: 1173179
+    }
+]
+
+// Logger la somme des populations de Paris et Dublin !
+
 export default function main(stage) {
-    let circle = new createjs.Shape();
-    circle.graphics.beginFill("#f19648").drawCircle(0, 0, 50);
-    circle.x = 100;
-    circle.y = 100;
+    let total = 0;
+    for (let indice = 0; indice < cities.length; indice++) {
+        let city = cities[indice];
+        total += city.population;
+    }
 
-    circle.addEventListener('click', (e) => {
-        circle.x += 5;
-    })
-    stage.addChild(circle);
-
-    return stage;
+    console.log(total);
 }
